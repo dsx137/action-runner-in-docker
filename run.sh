55e15244011c1c94d8f62034702312d8f478e61a
dsx137 <70027572+dsx137@users.noreply.github.com>
2025-07-07 14:41:03 +0800

    fix: auto replicas

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")"
+cd "$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")" || return 1
 
 source .env
 
@@ -17,3 +17,10 @@ if [ -z "$ORG_NAME" ]; then
 fi
 
 docker compose up --scale github-runner="$SCALE" -d
+
+echo "Starting Daemon..."
+while true; do
+    docker compose rm -f 2>/dev/null
+    docker compose up --scale github-runner="$SCALE" -d 2>&1 | grep -E 'Creating|Recreating|Restarting'
+    sleep 1
+done
